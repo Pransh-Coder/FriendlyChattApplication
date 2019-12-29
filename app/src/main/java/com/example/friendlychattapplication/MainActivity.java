5b08323484a8b1b0203bd2e08895fbc32d877820
Pransh-Coder <rastogipransh4@gmail.com>
2019-12-30 00:39:17 +0530

    Login Feature Added

--- app/src/main/java/com/example/friendlychattapplication/MainActivity.java
+++ app/src/main/java/com/example/friendlychattapplication/MainActivity.java
@@ -13,11 +13,15 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,6 +29,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EventListener;
 import java.util.List;
 
@@ -34,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    public static final int RC_SIGN_IN =1 ;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -48,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference mMessagesDatabaseRefrence;   //Database refrence obj    ( it reffers to a specific part of a database.
     ChildEventListener mchildEventListener;
 
+    //AuthStateListner is used to check the the state whether the user is signedIn or signedOut
+    FirebaseAuth mfirebaseAuth;
+    FirebaseAuth.AuthStateListener mauthStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +68,10 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseDatabase = FirebaseDatabase.getInstance();     //getInstance() - used to get instance of class  (main accesing point for accesing the database)
         //using this acess point we are going to acess to a specifc part of database
         mMessagesDatabaseRefrence = mFirebaseDatabase.getReference().child("messages");        //getReference() - Gets a refrence to root node. THESE "messages" is the ROOT NODE
+
+
+        // Initialize Firebase Auth object
+        mfirebaseAuth = FirebaseAuth.getInstance();
                                                                                                //child("messages") - and in this we only interested in the messages portion of database
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -104,8 +118,6 @@ public class MainActivity extends AppCompatActivity {
         mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
 
         // Send button sends a message and clears the EditText
-
-
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -158,6 +170,48 @@ public class MainActivity extends AppCompatActivity {
         };
         //The refrence defines what actually i'm listening to and listner obj derfines exactly what happens to the data
         mMessagesDatabaseRefrence.addChildEventListener(mchildEventListener);   //addChildEventListener()- Add a listener for child events ourring at this location. When child locations are added, removed, changed, or moved, the listener will be triggered for the appropriate event
+
+        mauthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //The diff b/w FirebaseAuth firebaseAuth and the one which we created(mfirebaseAuth) is that this FirebaseAuth parameter only contins that whether at that moment the user is authenticated or not
+
+                //FirebaseAuth firebaseAuth-  it contains 2 states either signedIn or signedOut
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user!=null){
+                    //User SignedIn
+                    Toast.makeText(MainActivity.this, "You are Signed In!!", Toast.LENGTH_SHORT).show();
+                }else {
+                    //User SignedOut
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)       //smartlock allows the phone to sort credentials and try to log them in
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);        //RC_SIGN_IN - it is a flag for when we return from starting the activity for result
+                }
+
+            }
+        };
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mfirebaseAuth.addAuthStateListener(mauthStateListener);     //add AuthState listner
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Removal of Authstate listner
+        mfirebaseAuth.removeAuthStateListener(mauthStateListener);
     }
 
     @Override
